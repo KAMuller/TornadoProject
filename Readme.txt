85cac70fdb196bcf38becfc0cc26cc4f187fd9ea
JB <jcbarker33@gmail.com>
2019-04-29 19:42:48 -0400

    Added Read me

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Legend for the Weather Data
+TMAX (Maximum Daily Air Temperature): Highest 5-minute averaged temperature observation reported each day.
+TMIN (Minimum Daily Air Temperature): Lowest 5-minute averaged temperature observation reported each day.
+TAVG (Average Air Temperature): Average of all 5-minute averaged temperature observations each day.
+DAVG (
--- TornadoPredictor.py
+++ TornadoPredictor.py
@@ -1,12 +1,17 @@
-
-
+# data processing
+# THings to do:
+# 1) get avg values form each station per day (for the whole area) 1 vector
+# 2) Make a list of each daily weather values
+# 3) The Training data is then the past 3 days prior. EXAMPLE: january 4th has the 1st, 2nd and 3rd.
+# 4) Class lables in a separate list
+# 5) Store The dates of the tornado
 
 
 
 
 
 #################################################################################
-
+# classifiers
 
 
 
