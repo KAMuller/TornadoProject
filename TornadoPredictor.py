9fe43b93ca759d657a988515aec9f029923e6a28
JB <jcbarker33@gmail.com>
2019-05-01 22:28:49 -0400

    Data processed!

--- TornadoPredictor.py
+++ TornadoPredictor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 import numpy as np
 # data processing
 # THings to do:
@@ -8,18 +9,119 @@ import numpy as np
 # 4) Class lables in a separate list
 # 5) Store The dates of the tornado
 # With Pandas, to use data frame returned by read_csv as 2d array use .values[][]
-weather_data = pd.read_csv('Weather_Data.csv', header=0)
-print(weather_data.head(1))
-norm_weather_data = pd.read_csv('NORM_weather.csv', header=0)
-print(norm_weather_data.head(1))
-#Get the first tornado dates
-T_McClain = pd.read_csv('Tornado_McClain_Train.csv', header=None)
-print(T_McClain.head(5))
-tornado_dates_m = T_McClain.values[3:-1]
-print(tornado_dates_m[0])
-tornado_dates_m = tornado_dates_m[:, 2:3]
-print(tornado_dates_m)
-#Get the second tornado dates
+weather_data = pd.read_csv('Weather_Data.csv', header=0, dtype={'STID': str})
+# print(weather_data.head(1))
+norm_weather_data = pd.read_csv('NORM_weather.csv', header=0, dtype={'STID': str})
+# Get the first tornado dates
+t_McClain = pd.read_csv('Tornado_McClain_Train.csv', header=None)
+tornado_dates_m = t_McClain.values[3:-1, 2:3]
+
+# Get the second tornado dates
+t_Grady = pd.read_csv('Tornado_Grady_Train.csv', header=None)
+tornado_dates_g = t_Grady.values[3:-1, 2:3]
+
+# Get the third tornado dates
+t_Cleve = pd.read_csv('Tornado_Cleveland_Train.csv', header=None)
+tornado_dates_c = t_Cleve.values[3:-1, 2:3]
+
+
+# combining tornado dates
+tornadoes = []
+for x in tornado_dates_m:
+    if x not in tornadoes:
+        tornadoes.append(x)
+for x in tornado_dates_g:
+    if x not in tornadoes:
+        tornadoes.append(x)
+for x in tornado_dates_c:
+    if x not in tornadoes:
+        tornadoes.append(x)
+tornadoes = np.array(tornadoes)
+print("There are: ", len(tornadoes), "tornadoes.")
+
+# Combining Weather data
+weather_vectors = []
+weather_dates = []
+weather = weather_data.values.tolist()
+norm_weather = norm_weather_data.values.tolist()
+for x in weather:
+    year = x[0]
+    month = x[1]
+    day = x[2]
+    date = str(month) + '/' + str(day) + '/' + str(year)
+    if date not in weather_dates:
+        weather_dates.append(date)
+    weather_vectors.append(x[4:-1])
+
+nan_count = 0
+i = 0
+for x in weather_vectors:
+    i += 1
+    nan_count = 0
+    for y in range(len(weather_vectors[0])):
+        if x[y] < -1:
+            nan_count += 1
+            x[y] = -1
+    if nan_count == len(x):
+        weather_vectors[i-1] = 0
+# print(weather_vectors[:5])
+i = 0
+for x in norm_weather:
+    i += 1
+    nan_count = 0
+    for y in range(len(norm_weather[0])):
+        if type(x[y]) != str:
+            if x[y] < -1:
+                nan_count += 1
+                x[y] = -1
+    if nan_count == len(x):
+        norm_weather[i-1] = 0
+# print("Norm Weather is: ", norm_weather[:5], '\n')
+count = 0
+for x in range(len(weather_vectors)):
+    if weather_vectors[x] == 0:
+        newline = norm_weather[count]
+        weather_vectors[x] = newline[4:-1]
+        count += 1
+# print("The complete weather is: ", weather_vectors[:5])
+# Combining weather vectors
+weather_train = []
+hold = []
+i = 0
+while i < len(weather_vectors):
+    v1 = weather_vectors[i]
+    v2 = weather_vectors[i+1]
+    v3 = weather_vectors[i+2]
+    hold = [v1, v2, v3]
+    new_vector = np.mean(hold, axis=0)
+    new_vector = np.round(new_vector, decimals=4)
+    weather_train.append(new_vector)
+    i += 3
+weather_train = np.array(weather_train)
+# print(weather_train[:2])
+print("There are: ", len(weather_train), "vectors.")
+
+# Assign Labels
+print("There are: ", len(weather_dates), "dates.")
+weather_train_labels = []
+hold = []
+for x in range(len(weather_dates)):
+    hold = []
+    if weather_dates[x] in tornadoes:
+        hold.append(weather_train[x])
+        hold.append('Yes')
+        weather_train_labels.append(hold)
+    else:
+        hold.append(weather_train[x])
+        hold.append('No')
+        weather_train_labels.append(hold)
+tornado_count = 0
+for x in weather_train_labels:
+    if x[len(weather_train_labels[0])-1] == 'Yes':
+        tornado_count += 1
+print("There are ", tornado_count, "recorded tornadoes!")
+
+
 
 
 
